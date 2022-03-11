4ee2337ba1d12deabeb9324ed0cfda46f53253ea
QuentinGast <2061694@bdeb.qc.ca>
2022-03-11 18:13:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const connectionParams={
 }
 
 mongoose.connect(url,connectionParams).then( () => {
-    console.log('Connected to database ');
+    console.log('Connected to database');
 }).catch( (err) => {
     console.error(`Error connecting to the database. \n${err}`);
 })
