80e7545817355a1bae0a7eedd334d293d3ea45f1
QuentinGast <2061694@bdeb.qc.ca>
2022-03-25 17:44:58 -0400

    ajout logout

--- server.js
+++ server.js
@@ -385,6 +385,16 @@ app.post('/admin',async function (req,res) {
     res.end ( 'done' ) ;
 });
 
+app.get( '/logout' ,(req , res ) => {
+    req.session.destroy ( ( err ) => {
+        if ( err ) {
+           console.log( err ) ;
+        }
+        res.redirect ( '/' ) ;
+    } ) ;
+
+} ) ;
+
 /**
 * connect to server
 */
